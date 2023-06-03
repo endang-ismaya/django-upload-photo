1548ab63e03cba915a79faf7c1fb8059586dadb5
Endang Ismaya <endang.ismaya@gmail.com>
2023-06-03 14:01:35 +0700

    update apps.feeds

--- apps/feeds/models.py
+++ apps/feeds/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    text = models.CharField(max_length=140, blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return f"{self.text}"
