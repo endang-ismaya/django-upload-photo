9fb7ba778e83c00ee8370f87b2ef5226c5735fa0
Endang Ismaya <endang.ismaya@gmail.com>
2023-06-04 14:02:06 +0700

    add post-add page

--- apps/feeds/urls.py
+++ apps/feeds/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from apps.feeds.views import HomePageView, PostDetailView
+from apps.feeds.views import HomePageView, PostDetailView, PostAddView
 
 urlpatterns = [
     path("", HomePageView.as_view(), name="feeds__home"),
     path("details/<int:pk>/", PostDetailView.as_view(), name="feeds__detail"),
+    path("posts/add/", PostAddView.as_view(), name="feeds__addpost"),
 ]
--- apps/feeds/views.py
+++ apps/feeds/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView, DetailView
+from django.views.generic import TemplateView, DetailView, FormView
+from django.urls import reverse_lazy
 
+from apps.feeds.forms import PostForm
 from apps.feeds.models import Post
 
 
@@ -8,7 +10,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["posts"] = Post.objects.all()
+        context["posts"] = Post.objects.all().order_by("-created_at")
 
         return context
 
@@ -16,3 +18,15 @@ class HomePageView(TemplateView):
 class PostDetailView(DetailView):
     template_name = "feeds/detail.html"
     model = Post
+
+
+class PostAddView(FormView):
+    template_name = "feeds/post-add.html"
+    form_class = PostForm
+    success_url = reverse_lazy("feeds__home")
+
+    def form_valid(self, form):
+        new_post = Post.objects.create(
+            text=form.cleaned_data["text"], image=form.cleaned_data["image"]
+        )
+        return super().form_valid(form)
